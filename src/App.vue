b0c41d6c03d3f8a621eb5db1921a8c6b0bee309f
Victor Neves <info@victorneves.dev>
2022-01-06 21:12:31 +0000

    Format code, remove axios and replace by fetch within a try catch block and replace for by a for-of

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,65 @@
 <template>
   <div id="app">
-    <search @filterUser="filterUser"></search>
-    <spinner></spinner>
-    <users :usersList="usersListAux"></users>
+    <Search @filterUser="filterUser"></Search>
+    <Spinner />
+    <Users :usersList="usersListAux"></Users>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import Spinner from "./components/Spinner.vue";
-import Users from "./components/Users.vue";
-import Search from "./components/Search.vue";
+import Spinner from './components/Spinner.vue'
+import Users from './components/Users.vue'
+import Search from './components/Search.vue'
 
 export default {
-  name: "app",
+  name: 'app',
+
   components: {
     Spinner,
     Users,
     Search
   },
-  data() {
+
+  data () {
     return {
       usersList: [],
       usersListAux: []
-    };
+    }
   },
+
   methods: {
-    filterUser: function(search) {
-      this.usersListAux = [];
-      for (var i = 0; i < this.usersList.length; i++) {
-        if (this.usersList[i].name.toLowerCase().includes(search.toLowerCase())) {
-          this.usersListAux.push(this.usersList[i]);
+    /**
+     *
+     * @param {String} search
+     */
+    filterUser(search) {
+      this.usersListAux = []
+
+      for (let user of this.usersList) {
+        if (user.name.toLowerCase().includes(search.toLowerCase())) {
+          this.usersListAux.push(user)
         }
       }
     },
-    getUsers() {
-      axios
-        .get("https://jsonplaceholder.typicode.com/users")
-        .then(response => {
-          document.getElementById("js-spinner").style.opacity = "0";
-          document.getElementById("js-content").style.opacity = "1";
-          this.usersList = response.data;
-          this.usersListAux = response.data;
-        })
-        .catch(function(error) {
-          console.log("error", error);
-        });
+
+    async getUsers () {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+
+        document.getElementById('js-spinner').style.opacity = '0'
+        document.getElementById('js-content').style.opacity = '1'
+
+        this.usersList = data
+        this.usersListAux = data
+      } catch (error) {
+          throw new Error(error)
+      }
     }
   },
-  mounted() {
-    this.getUsers();
+
+  mounted () {
+    this.getUsers()
   }
 }
 </script>
